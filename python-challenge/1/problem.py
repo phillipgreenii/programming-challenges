355be07bc91b187caf6af6421083ccdca5bf2962
Phillip Green II <deathcheese@yahoo.com>
2010-05-13 00:32:11 -0400

    completed problem 1

---
+++ python-challenge/1/problem.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+# http://www.pythonchallenge.com/pc/def/map.html
+
+import string
+
+input = "g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj."
+
+def translate(str):
+    return str.translate(string.maketrans('abcdefghijklmnopqrstuvwxyz','cdefghijklmnopqrstuvwxyzab'),'')
+
+print translate(input)
+
+print translate('map')
