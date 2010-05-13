ee4e17eda5d939ab13a12f786faed2c6c4c760a6
Phillip Green II <deathcheese@yahoo.com>
2010-05-13 00:53:06 -0400

    completed problem 2

---
+++ python-challenge/2/problem.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+# http://www.pythonchallenge.com/pc/def/ocr.html
+
+import string
+
+letters = list('abcdefghijklmnopqrstuvwxyz')
+for line in open('input.file'):
+    for letter in list(line.strip()):
+        if letter in letters:
+            print letter,
